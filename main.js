135f0d5276152775f47b0e140bdbab4f0a290591
AZ <wira123widodo@gmail.com>
2022-08-29 23:43:10 +0700

    style[AW]: dashboard revision

--- main.js
+++ main.js
@@ -12,31 +12,7 @@ function carousel() {
   slideIndex++;
   if (slideIndex > x.length) {slideIndex = 1} 
   x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 1000); 
+  setTimeout(carousel, 2500); 
 }
 
 // end //
-
-// frogres scrolling animation
-
-function progress() {
-    var windowScrollTop = $(window).scrollTop();
-    var docHeight = $(document).height();
-    var windowHeight = $(window).height();
-    var progress = (windowScrollTop / (docHeight - windowHeight)) * 100;
-
-    var $bgColor = progress > 99 ? "#fff" : "#fff";
-    var $textColor = progress > 99 ? "#fff" : "#333";
-
-    $("h1")
-        .text(Math.round(progress) + "%")
-        .css({ color: $textColor });
-
-    $(".fill")
-        .height(progress + "%")
-        .css({ backgroundColor: $bgColor });
-}
-progress();
-$(document).on("scroll", progress);
-
-// 
