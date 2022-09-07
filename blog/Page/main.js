af9cb5dedadebd5eb1371eae24889c29d9cf5b9d
Akbar Rizqullah <akbarrizqullah228@gmail.com>
2022-09-07 17:33:51 +0700

    style: [AR] add navbar and add content in blog

--- blog/Page/main.js
+++ blog/Page/main.js
@@ -1,47 +1,66 @@
-document.querySelector("#mds1").addEventListener("click", () => {
-    window.location.href = "/blog/Page/berusaha.html";
-});
+//NavbarScroll
+const jumpTo = (id) => {
+  document.getElementsByClassName(id)[0].scrollIntoView({
+    behavior: "smooth",
+  });
+};
 
-document.querySelector("#mds2").addEventListener("click", () => {
-    window.location.href = "/blog/Page/berhenti.html";
-});
+var slideIndex = 0;
+carousel();
 
-document.querySelector("#mds3").addEventListener("click", () => {
-    window.location.href = "/blog/Page/memiliki.html";
-});
+//end NavbarScroll
 
-document.querySelector("#mds4").addEventListener("click", () => {
-    window.location.href = "/blog/Page/jangan.html";
-});
+//event listener
+document.getElementById("beranda").onclick = function () {
+  location.href = "/index.html";
+};
 
-document.querySelector("#mds5").addEventListener("click", () => {
-    window.location.href = "/blog/Page/percaya.html";
-});
+document.getElementById("mds1").onclick = function () {
+  location.href = "/blog/Page/berusaha.html";
+};
 
-document.querySelector("#mds6").addEventListener("click", () => {
-    window.location.href = "/blog/Page/mengenali.html";
-});
+document.getElementById("mds2").onclick = function () {
+  location.href = "/blog/Page/berhenti.html";
+};
 
-document.querySelector("#mds7").addEventListener("click", () => {
-    window.location.href = "/blog/Page/fokus-pada-kebahagiaan-dirimu.html";
-});
+document.getElementById("mds3").onclick = function () {
+  location.href = "/blog/Page/memiliki.html";
+};
 
-document.querySelector("#mds8").addEventListener("click", () => {
-    window.location.href = "/blog/Page/tidak.html";
-});
 
-document.querySelector("#mds9").addEventListener("click", () => {
-    window.location.href = "/blog/Page/fokus-pada-hal-positif.html";
-});
+document.getElementById("mds4").onclick = function () {
+  location.href = "/blog/Page/jangan.html";
+};
 
-document.querySelector("#mds10").addEventListener("click", () => {
-    window.location.href = "/blog/Page/fokus-pada-tujuan.html";
-});
+document.getElementById("mds5").onclick = function () {
+  location.href = "/blog/Page/percaya.html";
+};
 
-document.querySelector("#mds11").addEventListener("click", () => {
-    window.location.href = "/blog/Page/ekspresikan.html";
-});
+document.getElementById("mds6").onclick = function () {
+  location.href = "/blog/Page/mengenali.html";
+};
 
-document.querySelector("#mds12").addEventListener("click", () => {
-    window.location.href = "/blog/Page/cintai.html";
-});
+document.getElementById("mds7").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-kebahagiaan-dirimu.html";
+};
+
+document.getElementById("mds8").onclick = function () {
+  location.href = "/blog/Page/tidak.html";
+};
+
+document.getElementById("mds9").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-hal-positif.html";
+};
+
+document.getElementById("mds10").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-tujuan.html";
+};
+
+document.getElementById("mds11").onclick = function () {
+  location.href = "/blog/Page/ekspresikan.html";
+};
+
+document.getElementById("mds12").onclick = function () {
+  location.href = "/blog/Page/cintai.html";
+};
+
--- main.js
+++ main.js
@@ -26,4 +26,61 @@ function carousel() {
   setTimeout(carousel, 2500);
 }
 
+
+//event listener
+document.getElementById("beranda").onclick = function () {
+  location.href = "/index.html";
+};
+
+document.getElementById("mds1").onclick = function () {
+  location.href = "/blog/Page/berusaha.html";
+};
+
+document.getElementById("mds2").onclick = function () {
+  location.href = "/blog/Page/berhenti.html";
+};
+
+document.getElementById("mds3").onclick = function () {
+  location.href = "/blog/Page/memiliki.html";
+};
+
+
+document.getElementById("mds4").onclick = function () {
+  location.href = "/blog/Page/jangan.html";
+};
+
+document.getElementById("mds5").onclick = function () {
+  location.href = "/blog/Page/percaya.html";
+};
+
+document.getElementById("mds6").onclick = function () {
+  location.href = "/blog/Page/mengenali.html";
+};
+
+document.getElementById("mds7").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-kebahagiaan-dirimu.html";
+};
+
+document.getElementById("mds8").onclick = function () {
+  location.href = "/blog/Page/tidak.html";
+};
+
+document.getElementById("mds9").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-hal-positif.html";
+};
+
+document.getElementById("mds10").onclick = function () {
+  location.href = "/blog/Page/fokus-pada-tujuan.html";
+};
+
+document.getElementById("mds11").onclick = function () {
+  location.href = "/blog/Page/ekspresikan.html";
+};
+
+document.getElementById("mds12").onclick = function () {
+  location.href = "/blog/Page/cintai.html";
+};
+
+
+
 // end //
