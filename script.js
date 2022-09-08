62a45d22b42c8d4439231f7cbfe6ee38e673f6a9
Viriyadhi <viriyadhitjan@gmail.com>
2022-09-08 16:40:15 +0700

    feat[VS]: On scroll animation

--- script.js
+++ script.js
@@ -88,4 +88,40 @@ function tabletResize(tablet) {
 tablet.addListener(tabletResize);
 tabletResize(tablet);
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+function revealMds() {
+    var reveals = document.querySelectorAll(".reveal-mds");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 0;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", revealMds);
+
 // end //
